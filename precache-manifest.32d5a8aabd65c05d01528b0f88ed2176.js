256e1de8ecc24c468d41fbfeee273835b4c94bb7
shivam232323 <66739415+shivam232323@users.noreply.github.com>
2020-12-06 21:52:24 +0530

    Updates

---
+++ precache-manifest.32d5a8aabd65c05d01528b0f88ed2176.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf76c00918091b74f9b8581c23cc9888",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "719f3f528c7516377a9e",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "db35ab7b81fe87c7e8b7",
+    "url": "/face-recognition/static/css/main.de5a5354.chunk.css"
+  },
+  {
+    "revision": "719f3f528c7516377a9e",
+    "url": "/face-recognition/static/js/2.464164f7.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/face-recognition/static/js/2.464164f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db35ab7b81fe87c7e8b7",
+    "url": "/face-recognition/static/js/main.e2093814.chunk.js"
+  },
+  {
+    "revision": "6466779e50babf1762cc",
+    "url": "/face-recognition/static/js/runtime-main.401179d7.js"
+  },
+  {
+    "revision": "b114960fa87a1fa7fa9c3f3cc9b84258",
+    "url": "/face-recognition/static/media/smart.b114960f.jpg"
+  }
+]);
